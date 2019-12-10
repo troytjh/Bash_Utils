db3ce645797dffccb74de86f35ff39f094487e62
troytjh <troytjh98@gmail.com>
2019-12-10 12:39:46 -0600

    12/10/19

---
+++ Command_File_References/Commands.md
@@ -0,0 +1,20 @@
+﻿# Task Management
+	lsof		give running processes and their related file paths 
+
+# git commands
+    git worktree add ${File_Path} ${Branch_Name}
+        allows for multiple branches to be checkout at the same time
+            File_Path: path to store the branch
+
+            Branch_Name: name of the branch to add to workspace
+
+# nextcloud maintenance mode
+    sudo -u http php occ maintenance:mode --on
+
+    **app maintenance commands**
+        sudo -u http php occ app:update app_name
+
+
+
+# file management
+    nnn -dHS
--- lt2dk/lt2dk.sh
+++ lt2dk/lt2dk.sh
@@ -5,17 +5,19 @@ OUT=${FILE%.*}
 OUT="${OUT// /_}"
 
 cd "$WORKDIR"
-mkdir img
-pdftoppm -r 150 "$FILE" img/${OUT} -png
+mkdir .img
+pdftoppm -r 150 "$FILE" .img/${OUT} -png
 
-len=$(pdfinfo "$FILE" | grep Pages | awk '{print $2}')
+len=$(pdfinfo "$FILE" | grep -a Pages | awk '{print $2}')
 for i in $(seq -w 1 $len)
 do
-    ffmpeg -i img/${OUT}-${i}.png -hide_banner -loglevel panic -vf negate,eq=brightness=.25:contrast=1.15,colorkey=white -c:a copy img/${OUT}_dark-${i}.png
-    ffmpeg -i img/${OUT}_dark-${i}.png -hide_banner -loglevel panic -filter_complex "[0]split=2[bg][fg];[bg]drawbox=c=0xE1E1E1@1:replace=1:t=fill[bg]; [bg][fg]overlay=format=auto" -c:a copy img/${OUT}_dark-${i}_${i}.png
-    rm img/${OUT}-${i}.png
-    rm img/${OUT}_dark-${i}.png
+    ffmpeg -i .img/${OUT}-${i}.png -hide_banner -loglevel panic -vf negate,eq=brightness=.25:contrast=1.15,colorkey=white -c:a copy .img/${OUT}_dark-${i}.png
+    ffmpeg -i .img/${OUT}_dark-${i}.png -hide_banner -loglevel panic -filter_complex "[0]split=2[bg][fg];[bg]drawbox=c=0xE1E1E1@1:replace=1:t=fill[bg]; [bg][fg]overlay=format=auto" -c:a copy .img/${OUT}_dark-${i}_${i}.png
+    rm .img/${OUT}-${i}.png
+    rm .img/${OUT}_dark-${i}.png
 done
 
-convert -limit memory 3000 -limit map 3000 -limit area 10000 img/*.png -density 150 ${OUT}_dark.pdf
-rm -r img
+#export MAGICK_TEMPORARY_PATH=/Drive/.cache
+#export MAGICK_TMPDIR=/Drive/.cache
+convert -limit memory 3000 -limit map 3000 .img/*.png -density 150 ${OUT}_dark.pdf
+rm -r .img
